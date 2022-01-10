390317d6da36d0dd38441085b35f767a8f2de1e3
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-01-10 13:34:58 +0100

    first commit

---
+++ hello.js
@@ -0,0 +1,5 @@
+function sayHello(name = "World"){
+    console.log(`Hello, ${name}!`);
+}
+
+export default sayHello;
---
+++ main.js
@@ -0,0 +1,6 @@
+//skicka med function
+import sayHello from "./hello";
+sayHello("Emelie"); 
+
+//skicka med js fil
+import "./hello";
